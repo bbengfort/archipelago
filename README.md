566a7316562222a06620caacb3887193b4e4e17f
Benjamin Bengfort <benjamin@bengfort.com>
2017-03-28 17:46:24 -0400

    alia and managment commands

--- README.md
+++ README.md
@@ -16,4 +16,54 @@ You can then run commands as follows:
 $ fab hostname
 ```
 
-And they will execute on the entire cluster. 
+And they will execute on the entire cluster.
+
+## Basics
+
+Run the deploy command to pull the latest version of the project:
+
+```
+$ fab deploy
+```
+
+This should run a bunch of commands on all hosts in parallel, pulling the most recent version of the repository and updating the dependencies with `godep`. The servers can all be run simultaneously as follows:
+
+```
+$ fab serve
+```
+
+This command will run the alia serve command in the background using supervisord. The replicas can then be shutdown using the shutdown command:
+
+```
+$ fab shutdown
+```
+
+Finally the data (the replicated logs) can be ingested to a local directory as follows:
+
+```
+$ fab ingest:path/to/logs
+```
+
+Note that this command will download the logs from every host, saving them with their associated host name.
+
+## Management
+
+There are a couple of management tasks implemented:
+
+```
+$ fab copyto:local,remote
+```
+
+Copies a file from the local path to the remote path on all hosts. If administrative permissions are required:
+
+```
+$ fab --sudo-password $PASSWORD copyto:local,remote,sudo=True
+```
+
+Finally you can apt-get install packages on all machines with:
+
+```
+$ fab --sudo-password $PASSWORD aptget:pkga,pkgb,pkgc
+```
+
+Use with care though, as this may require input from the user. 
--- fabfile.py
+++ fabfile.py
@@ -18,7 +18,7 @@ Fabric command definitions for Archipelago management.
 ##########################################################################
 
 from os import path
-from fabric.api import env, run, cd, parallel, settings
+from fabric.api import env, run, cd, parallel, settings, put, sudo
 
 
 ##########################################################################
@@ -28,11 +28,12 @@ from fabric.api import env, run, cd, parallel, settings
 # Load hosts from a private hosts file
 with open('hosts.txt', 'r') as hosts:
     env.hosts = [
-        "bengfort@{}".format(host.strip())
+        host.strip()
         for host in hosts
-        if host.strip != ""
+        if host.strip() != ""
     ]
 
+env.user = "bengfort"
 env.forward_agent = True
 
 # Project repository
@@ -46,9 +47,10 @@ entries = "/data/alia/entries.log"
 
 
 ##########################################################################
-## Commands
+## Alia Commands
 ##########################################################################
 
+@parallel
 def deploy():
     """
     Pull the most recent version of the repository and source the profile
@@ -56,17 +58,56 @@ def deploy():
     with cd(project):
         run("git pull")
         run("godep restore")
-        run(". {}".format(profile))
+        run("go install ./cmd/alia/")
 
 
+@parallel
 def serve():
     """
     Run the alia consensus group replica servers
     """
-    with cd(project):
-        run("echo $GOPATH")
+    raise NotImplementedError("not yet implemented")
 
 
+@parallel
+def shutdown():
+    """
+    Shutdown the alia consensus group replica servers
+    """
+    raise NotImplementedError("not yet implemented")
+
+
+@parallel
+def ingest(outpath="."):
+    """
+    Collect the logs from all the remotes and save them locally
+    """
+    local_path = path.join(outpath, "entries.%(host)s.log")
+    get(entries, local_path)
+
+
+##########################################################################
+## Cluster Management Commands
+##########################################################################
+
+@parallel
+def copyto(localpath, remotepath, sudo=False):
+    """
+    Copy a file from the local to the remote
+    """
+    put(localpath, remotepath, use_sudo=sudo)
+
+
+@parallel
+def aptget(*packages):
+    """
+    Install packages using apt-get install using sudo.
+    """
+    packages = " ".join(packages)
+    sudo("apt-get install -y {}".format(packages))
+
+
+@parallel
 def hostname():
     """
     Prints out the hostname of all machines
